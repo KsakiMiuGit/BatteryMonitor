86a7dd0749815cedf437d177f3837e1d07dab37a
KsakiMiuGit <baumert.karolina@gmail.com>
2024-09-14 23:54:30 +0200

    Dodaj pliki projektów.

---
+++ BatterMonitor/BatteryMonitor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatterMonitor
+{
+    public class BatteryMonitor
+    {
+        public delegate void BatteryLowEventHandler(object sender, EventArgs e);
+        public event BatteryLowEventHandler? BatteryLow;
+
+        private int _batteryLevel;
+
+        public BatteryMonitor(int initialLevel)
+        {
+            _batteryLevel = initialLevel;
+        }
+
+        public int BatteryLevel
+        {
+            get => _batteryLevel;
+            set
+            {
+                _batteryLevel = value;
+                if (_batteryLevel < 20)
+                {
+                    OnBatteryLow(EventArgs.Empty); 
+                }
+            }
+        }
+
+        protected virtual void OnBatteryLow(EventArgs e)
+        {
+            BatteryLow?.Invoke(this, e);
+        }
+    }
+}
---
+++ BatterMonitor/Program.cs
@@ -0,0 +1,15 @@
+﻿using BatterMonitor;
+
+BatteryMonitor batteryMonitor = new BatteryMonitor(100);
+
+batteryMonitor.BatteryLow += (sender, e) =>
+{
+    Console.WriteLine("Warning: Battery level is critically low!");
+};
+
+for (int i = 100; i >= 0; i -= 10)
+{
+    Console.WriteLine($"Battery Level: {i}%");
+    batteryMonitor.BatteryLevel = i;
+    System.Threading.Thread.Sleep(500); 
+}
